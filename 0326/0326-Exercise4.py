6a58a3c28fc962e3db855e7b60add4ef22465ea4
RainingNight <36830427+RainingNight0329@users.noreply.github.com>
2018-03-26 20:31:40 +0800

    Exercise4

---
+++ 0326/0326-Exercise4.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+from source import calculator as c
+
+while True:
+    try:
+        x=input("請輸入數字（用空格分開）：").split(" ")
+        cal=input("請輸入要做的運算符號(+,-,*,/)：")
+        for i in range(0,len(x)):
+            x[i] = eval(x[i])
+        print(c.calculator(cal,x))
+        break
+    except:
+        print("輸入錯誤,請重新輸入")
+
